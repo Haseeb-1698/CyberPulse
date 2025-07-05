b3f1f8933547442c5d8c1fc22aa9aa920bbe51f8
Muhammad haseeb <i221698@nu.edu.pk>
2025-07-05 20:21:25 +0500

    Initial commit: VMS with Git LFS

---
+++ static/data/generate_remediation_data.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8351e833e89807a08ed783c2a8b0242bc7e6099b51d122a15e04ba22058ffd32
+size 35871
---
+++ static/data/structured_remediation_enhanced.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d29e07aa3d442eac44f0b6393042337dcd5c11fdc929f8830e2ee884cd9a4e19
+size 12322
